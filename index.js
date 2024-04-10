352d886d081e13ebc88b9ccb266610dcaa263b10
arevikmosinyan <aarreevviikk1@gmail.com>
2024-04-11 00:26:29 +0400

    css and js

--- index.js
+++ index.js
@@ -1,70 +1,54 @@
-let fromDown = false;
-let fromRight = false;
-let count = 0;
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+let dronesContainer = document.getElementById('dronesContainer');
+let positionX = 0;
+let positionY = 0;
+const step = 10;
 
 document.addEventListener('keydown', function (event) {
   let droneSet = document.querySelectorAll('.drone');
   let drone1 = droneSet[0];
   let drone2 = droneSet[1];
-  let dronesContainer = document.getElementById('dronesContainer');
-  let currentTop1 = parseInt(drone1.style.top) || 0;
-  let currentTop2 = parseInt(drone2.style.top) || 0;
 
   const step = 10;
+  // //rotate the drone to the left
+  // if (event.key === 'q') {
+  //   dronesContainer.style.transform = 'rotate(90deg)';
+  // }
+  // //rotate the drone to the roght
+  // if (event.key === 'e') {
+  //   dronesContainer.style.transform = 'rotate(-90deg)';
+  // }
 
-  if (event.key === 'ArrowLeft') {
-    dronesContainer.classList.remove(
-      'rotate-up',
-      'rotate-right',
-      'rotate-down',
-      // 'rotate-down-from-right',
-      // 'rotate-right-from-down',
-    );
-
-    dronesContainer.classList.add('rotate-left');
+  if (event.key === 'ArrowLeft' || event.key === 'a') {
+    if (positionX - 60 > 0) {
+      positionX -= step;
+      dronesContainer.style.left = positionX + 'px';
+      dronesContainer.style.transform = 'rotate(-270deg)';
+    }
   }
-  if (event.key === 'ArrowUp') {
-    dronesContainer.classList.remove(
-      'rotate-left',
-      'rotate-right',
-      'rotate-down',
-      // 'rotate-down-from-right',
-      // 'rotate-right-from-down',
-    );
-    dronesContainer.classList.add('rotate-up');
+  if (event.key === 'ArrowUp' || event.key === 'w') {
+    if (positionY > 20) {
+      positionY -= step;
+      dronesContainer.style.top = positionY + 'px';
+      dronesContainer.style.transform = 'rotate(-180deg)';
+    }
   }
-  if (event.key === 'ArrowRight') {
-    // fromRight = true;
-    dronesContainer.classList.remove(
-      'rotate-up',
-      'rotate-left',
-      'rotate-down',
-      // 'rotate-down-from-right',
-    );
-    // if (fromDown) {
-    //   dronesContainer.classList.add('rotate-right-from-down');
-    // } else {
-    dronesContainer.classList.add('rotate-right');
-    // }
+  if (event.key === 'ArrowRight' || event.key === 'd') {
+    if (positionX + dronesContainer.offsetHeight + 50 < windowWidth) {
+      console.log(dronesContainer.offsetWidth + 'dronesContainer.offsetWidth');
+      console.log(drone2.offsetWidth + 'drone2.offsetWidth');
+      console.log(drone1.offsetWidth + 'drone1.offsetWidth');
+      positionX += step;
+      dronesContainer.style.left = positionX + 'px';
+      dronesContainer.style.transform = 'rotate(-90deg)';
+    }
   }
-  if (event.key === 'ArrowDown') {
-    // fromDown = true;
-    dronesContainer.classList.remove(
-      'rotate-right',
-      'rotate-left',
-      'rotate-up',
-      // 'rotate-right-from-down',
-    );
-    // if (fromRight) {
-    //   dronesContainer.classList.add('rotate-down-from-right');
-    // } else {
-    dronesContainer.classList.add('rotate-down');
-    // }
-    //setTimeout(() => dronesContainer.classList.remove('rotate-down'), 500);
+  if (event.key === 'ArrowDown' || event.key === 's') {
+    if (positionY + dronesContainer.offsetHeight + 50 < windowHeight) {
+      positionY += step;
+      dronesContainer.style.top = positionY + 'px';
+      dronesContainer.style.transform = 'rotate(0deg)';
+    }
   }
-
-  currentTop1 += step;
-  currentTop2 += step;
-  drone1.style.top = currentTop1 + 'px';
-  drone2.style.top = currentTop2 + 'px';
 });
