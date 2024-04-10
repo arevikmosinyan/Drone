248ecae10c0ea47b5bc08424c60f674f4f9da71a
arevikmosinyan <aarreevviikk1@gmail.com>
2024-04-10 16:45:15 +0400

    css and js changes

--- index.js
+++ index.js
@@ -1,35 +1,70 @@
+let fromDown = false;
+let fromRight = false;
+let count = 0;
+
 document.addEventListener('keydown', function (event) {
   let droneSet = document.querySelectorAll('.drone');
   let drone1 = droneSet[0];
   let drone2 = droneSet[1];
-
+  let dronesContainer = document.getElementById('dronesContainer');
   let currentTop1 = parseInt(drone1.style.top) || 0;
-  let currentLeft1 = parseInt(drone1.style.left) || 0;
   let currentTop2 = parseInt(drone2.style.top) || 0;
-  let currentLeft2 = parseInt(drone2.style.left) || 0;
 
   const step = 10;
 
-  if (event.key === 'ArrowUp') {
-    currentTop1 -= step;
-    currentTop2 -= step;
-  }
-  if (event.key === 'ArrowDown') {
-    currentTop1 += step;
-    currentTop2 += step;
-  }
   if (event.key === 'ArrowLeft') {
-    currentLeft1 -= step;
-    currentLeft2 -= step;
+    dronesContainer.classList.remove(
+      'rotate-up',
+      'rotate-right',
+      'rotate-down',
+      // 'rotate-down-from-right',
+      // 'rotate-right-from-down',
+    );
+
+    dronesContainer.classList.add('rotate-left');
+  }
+  if (event.key === 'ArrowUp') {
+    dronesContainer.classList.remove(
+      'rotate-left',
+      'rotate-right',
+      'rotate-down',
+      // 'rotate-down-from-right',
+      // 'rotate-right-from-down',
+    );
+    dronesContainer.classList.add('rotate-up');
   }
   if (event.key === 'ArrowRight') {
-    currentLeft1 += step;
-    currentLeft2 += step;
+    // fromRight = true;
+    dronesContainer.classList.remove(
+      'rotate-up',
+      'rotate-left',
+      'rotate-down',
+      // 'rotate-down-from-right',
+    );
+    // if (fromDown) {
+    //   dronesContainer.classList.add('rotate-right-from-down');
+    // } else {
+    dronesContainer.classList.add('rotate-right');
+    // }
+  }
+  if (event.key === 'ArrowDown') {
+    // fromDown = true;
+    dronesContainer.classList.remove(
+      'rotate-right',
+      'rotate-left',
+      'rotate-up',
+      // 'rotate-right-from-down',
+    );
+    // if (fromRight) {
+    //   dronesContainer.classList.add('rotate-down-from-right');
+    // } else {
+    dronesContainer.classList.add('rotate-down');
+    // }
+    //setTimeout(() => dronesContainer.classList.remove('rotate-down'), 500);
   }
 
+  currentTop1 += step;
+  currentTop2 += step;
   drone1.style.top = currentTop1 + 'px';
-  drone1.style.left = currentLeft1 + 'px';
-
   drone2.style.top = currentTop2 + 'px';
-  drone2.style.left = currentLeft2 + 'px';
 });
