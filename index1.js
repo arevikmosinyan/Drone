8855b78d4b0d6c20c3ec9a6c446478292a8ceb30
arevikmosinyan <aarreevviikk1@gmail.com>
2024-04-12 22:18:56 +0400

    index.js

--- index1.js
+++ index1.js
@@ -8,28 +8,23 @@ let smaller = false;
 let rotationDegree = 0;
 let scaled = false;
 
-document.addEventListener("DOMContentLoaded", function () {
-  const popUp = document.getElementById("popUp");
-  const closeButton = document.getElementsByClassName("close")[0];
-  popUp.style.display = "block";
+document.addEventListener('DOMContentLoaded', function () {
+  const popUp = document.getElementById('popUp');
+  const closeButton = document.getElementsByClassName('close')[0];
+  popUp.style.display = 'block';
   closeButton.onclick = function () {
-    popUp.style.display = "none";
+    popUp.style.display = 'none';
   };
 
   window.onclick = function (event) {
     if (event.target == popUp) {
-      popUp.style.display = "none";
+      popUp.style.display = 'none';
     }
   };
 });
 
-<<<<<<< HEAD
 document.addEventListener('keydown', function (event) {
-  const step = 10;
-=======
-document.addEventListener("keydown", function (event) {
   const step = 7;
->>>>>>> a40ad126e33071ed420b375068c3b2b4efac4760
 
   /*-------------------------------making bigger and smaller to make the drone going up and landing-------------------------*/
 
@@ -64,25 +59,8 @@ document.addEventListener("keydown", function (event) {
   );
   let rotationDegree = Number(rotationDegreeString);
   // rotate the drone to the left
-<<<<<<< HEAD
-  if (event.key === 'q') {
-    if (rotationDegree === 0) {
-      dronesContainer.style.transform = 'rotate(-90deg)';
-    }
-    if (rotationDegree === -90) {
-      dronesContainer.style.transform = 'rotate(-180deg)';
-    }
-    if (rotationDegree === -180) {
-      dronesContainer.style.transform = 'rotate(-270deg)';
-    }
-    if (rotationDegree === -270) {
-      dronesContainer.style.transform = 'rotate(-360deg)';
-    }
-    if (rotationDegree === -360) {
-      rotationDegree = 0;
-=======
 
-  if (event.key === "q") {
+  if (event.key === 'q') {
     let rotationDegree = 0;
     findRotation();
     // let startIndex = dronesContainer.style.transform.indexOf("(") + 1;
@@ -96,13 +74,13 @@ document.addEventListener("keydown", function (event) {
     /*-----------------------------------------finding out if the drone is scaled or not, to make it moved with scaled transformation-------*/
 
     const startIndexOfScaleValue =
-      dronesContainer.style.transform.indexOf("scale(");
+      dronesContainer.style.transform.indexOf('scale(');
 
-    const endIndexOfScaleValue = dronesContainer.style.transform.indexOf(")");
+    const endIndexOfScaleValue = dronesContainer.style.transform.indexOf(')');
 
     const scaledSizeString = dronesContainer.style.transform.slice(
-      startIndexOfScaleValue + "scale(".length,
-      endIndexOfScaleValue
+      startIndexOfScaleValue + 'scale('.length,
+      endIndexOfScaleValue,
     );
 
     // console.log("Scaled size string:", scaledSizeString);
@@ -113,14 +91,13 @@ document.addEventListener("keydown", function (event) {
       rotationDegree = Math.abs(rotationDegree) + 90;
 
       return -rotationDegree;
->>>>>>> a40ad126e33071ed420b375068c3b2b4efac4760
     }
     console.log(findRotation());
     findRotation();
     dronesContainer.style.transform = `rotate(${findRotation()})`;
     dronesContainer.style.transform;
     console.log(
-      dronesContainer.style.transform + " dronesContainer.style.transform "
+      dronesContainer.style.transform + ' dronesContainer.style.transform ',
     );
     // if (rotationDegree === 0) {
     //   dronesContainer.style.transform = "rotate(-90deg)";
@@ -175,177 +152,138 @@ document.addEventListener("keydown", function (event) {
   // }
 
   //rotate the drone to the right
-<<<<<<< HEAD
   if (event.key === 'e') {
-    if (rotationDegree === 0) {
-      dronesContainer.style.transform = 'rotate(90deg)';
-    }
-    if (rotationDegree === 90) {
-      dronesContainer.style.transform = 'rotate(180deg)';
-    }
-    if (rotationDegree === 180) {
-      dronesContainer.style.transform = 'rotate(270deg)';
-    }
-    if (rotationDegree === 270) {
-      dronesContainer.style.transform = 'rotate(360deg)';
-    }
-    if (rotationDegree === 360) {
-=======
-  if (event.key === "e") {
-    let startIndex = dronesContainer.style.transform.indexOf("(") + 1;
-    let endIndex = dronesContainer.style.transform.indexOf("deg");
+    let startIndex = dronesContainer.style.transform.indexOf('(') + 1;
+    let endIndex = dronesContainer.style.transform.indexOf('deg');
     let rotationDegreeString = dronesContainer.style.transform.slice(
       startIndex,
-      endIndex
+      endIndex,
     );
     rotationDegree = Number(rotationDegreeString);
 
     if (rotationDegree === 0) {
-      dronesContainer.style.transform = "rotate(90deg)";
+      dronesContainer.style.transform = 'rotate(90deg)';
       rotationDegree = 90;
     } else if (rotationDegree === 90) {
-      dronesContainer.style.transform = "rotate(180deg)";
+      dronesContainer.style.transform = 'rotate(180deg)';
       rotationDegree = 180;
     } else if (rotationDegree === 180) {
-      dronesContainer.style.transform = "rotate(270deg)";
+      dronesContainer.style.transform = 'rotate(270deg)';
       rotationDegree = 270;
     } else if (rotationDegree === 270) {
-      dronesContainer.style.transform = "rotate(360deg)";
+      dronesContainer.style.transform = 'rotate(360deg)';
       rotationDegree = 360;
     } else if (rotationDegree === 360) {
->>>>>>> a40ad126e33071ed420b375068c3b2b4efac4760
       rotationDegree = 0;
     }
   }
   /*-------------------------------------------------------------------------------------------------------------------------*/
 
   /*--------------------------------------------------Arrow keys---------------------------------------------------------------*/
-<<<<<<< HEAD
-  if (event.key === 'ArrowLeft' || event.key === 'a') {
-    if (positionX - 60 > 0) {
-      positionX -= step;
-      dronesContainer.style.left = positionX + 'px';
-    }
-  } else if (event.key === 'ArrowUp' || event.key === 'w') {
-    if (positionY > 20) {
-      positionY -= step;
-      dronesContainer.style.top = positionY + 'px';
-    }
-  } else if (event.key === 'ArrowRight' || event.key === 'd') {
-    if (positionX + dronesContainer.offsetHeight + 50 < windowWidth) {
-      positionX += step;
-      dronesContainer.style.left = positionX + 'px';
-    }
-  } else if (event.key === 'ArrowDown' || event.key === 's') {
-    if (positionY + dronesContainer.offsetHeight + 50 < windowHeight) {
-      positionY += step;
-      dronesContainer.style.top = positionY + 'px';
-=======
 
   if (
     rotationDegree === 0 ||
     rotationDegree === -360 ||
     rotationDegree === 360
   ) {
-    if (event.key === "ArrowUp" || event.key === "w") {
+    if (event.key === 'ArrowUp' || event.key === 'w') {
       if (positionY + dronesContainer.offsetHeight + 50 < windowHeight) {
         positionY += step;
-        dronesContainer.style.top = positionY + "px";
+        dronesContainer.style.top = positionY + 'px';
       }
-    } else if (event.key === "ArrowDown" || event.key === "s") {
+    } else if (event.key === 'ArrowDown' || event.key === 's') {
       if (positionY > 20) {
         positionY -= step;
-        dronesContainer.style.top = positionY + "px";
+        dronesContainer.style.top = positionY + 'px';
       }
-    } else if (event.key === "ArrowRight" || event.key === "d") {
+    } else if (event.key === 'ArrowRight' || event.key === 'd') {
       if (positionX - 60 > 0) {
         positionX -= step;
-        dronesContainer.style.left = positionX + "px";
+        dronesContainer.style.left = positionX + 'px';
       }
-    } else if (event.key === "ArrowLeft" || event.key === "a") {
+    } else if (event.key === 'ArrowLeft' || event.key === 'a') {
       if (positionX + dronesContainer.offsetHeight + 50 < windowWidth) {
         positionX += step;
-        dronesContainer.style.left = positionX + "px";
+        dronesContainer.style.left = positionX + 'px';
       }
->>>>>>> a40ad126e33071ed420b375068c3b2b4efac4760
     }
   }
 
   if (rotationDegree === -90 || rotationDegree === 270) {
-    if (event.key === "ArrowUp" || event.key === "w") {
+    if (event.key === 'ArrowUp' || event.key === 'w') {
       if (positionX + dronesContainer.offsetHeight + 50 < windowWidth) {
         positionX += step;
-        dronesContainer.style.left = positionX + "px";
+        dronesContainer.style.left = positionX + 'px';
       }
     }
-    if (event.key === "ArrowDown" || event.key === "s") {
+    if (event.key === 'ArrowDown' || event.key === 's') {
       if (positionX - 60 > 0) {
         positionX -= step;
-        dronesContainer.style.left = positionX + "px";
+        dronesContainer.style.left = positionX + 'px';
       }
     }
-    if (event.key === "ArrowRight" || event.key === "d") {
+    if (event.key === 'ArrowRight' || event.key === 'd') {
       if (positionY + dronesContainer.offsetHeight + 50 < windowHeight) {
         positionY += step;
-        dronesContainer.style.top = positionY + "px";
+        dronesContainer.style.top = positionY + 'px';
       }
     }
-    if (event.key === "ArrowLeft" || event.key === "a") {
+    if (event.key === 'ArrowLeft' || event.key === 'a') {
       if (positionY > 20) {
         positionY -= step;
-        dronesContainer.style.top = positionY + "px";
+        dronesContainer.style.top = positionY + 'px';
       }
     }
   }
   if (rotationDegree === -180 || rotationDegree === 180) {
-    if (event.key === "ArrowUp" || event.key === "w") {
+    if (event.key === 'ArrowUp' || event.key === 'w') {
       if (positionY > 20) {
         positionY -= step;
-        dronesContainer.style.top = positionY + "px";
+        dronesContainer.style.top = positionY + 'px';
       }
     }
-    if (event.key === "ArrowDown" || event.key === "s") {
+    if (event.key === 'ArrowDown' || event.key === 's') {
       if (positionY + dronesContainer.offsetHeight + 50 < windowHeight) {
         positionY += step;
-        dronesContainer.style.top = positionY + "px";
+        dronesContainer.style.top = positionY + 'px';
       }
     }
-    if (event.key === "ArrowRight" || event.key === "d") {
+    if (event.key === 'ArrowRight' || event.key === 'd') {
       if (positionX + dronesContainer.offsetHeight + 50 < windowWidth) {
         positionX += step;
-        dronesContainer.style.left = positionX + "px";
+        dronesContainer.style.left = positionX + 'px';
       }
     }
-    if (event.key === "ArrowLeft" || event.key === "a") {
+    if (event.key === 'ArrowLeft' || event.key === 'a') {
       if (positionX - 60 > 0) {
         positionX -= step;
-        dronesContainer.style.left = positionX + "px";
+        dronesContainer.style.left = positionX + 'px';
       }
     }
   }
   if (rotationDegree === -270 || rotationDegree === 90) {
-    if (event.key === "ArrowUp" || event.key === "w") {
+    if (event.key === 'ArrowUp' || event.key === 'w') {
       if (positionX - 60 > 0) {
         positionX -= step;
-        dronesContainer.style.left = positionX + "px";
+        dronesContainer.style.left = positionX + 'px';
       }
     }
-    if (event.key === "ArrowDown" || event.key === "s") {
+    if (event.key === 'ArrowDown' || event.key === 's') {
       if (positionX + dronesContainer.offsetHeight + 50 < windowWidth) {
         positionX += step;
-        dronesContainer.style.left = positionX + "px";
+        dronesContainer.style.left = positionX + 'px';
       }
     }
-    if (event.key === "ArrowRight" || event.key === "d") {
+    if (event.key === 'ArrowRight' || event.key === 'd') {
       if (positionY > 20) {
         positionY -= step;
-        dronesContainer.style.top = positionY + "px";
+        dronesContainer.style.top = positionY + 'px';
       }
     }
-    if (event.key === "ArrowLeft" || event.key === "a") {
+    if (event.key === 'ArrowLeft' || event.key === 'a') {
       if (positionY + dronesContainer.offsetHeight + 50 < windowHeight) {
         positionY += step;
-        dronesContainer.style.top = positionY + "px";
+        dronesContainer.style.top = positionY + 'px';
       }
     }
   }
@@ -354,36 +292,36 @@ document.addEventListener("keydown", function (event) {
 
   /*--------------------------------------------------rotation vizualisation of the wings----------------------------------------------------*/
 
-  let drone1WingRight = document.querySelector("#drone1WingRight");
-  let drone1WingLeft = document.querySelector("#drone1WingLeft");
-  let drone2WingRight = document.querySelector("#drone2WingRight");
-  let drone2WingLeft = document.querySelector("#drone2WingLeft");
+  let drone1WingRight = document.querySelector('#drone1WingRight');
+  let drone1WingLeft = document.querySelector('#drone1WingLeft');
+  let drone2WingRight = document.querySelector('#drone2WingRight');
+  let drone2WingLeft = document.querySelector('#drone2WingLeft');
 
-  let newDuration = "0.1s";
-  let defaultDuration = "0.4s";
+  let newDuration = '0.1s';
+  let defaultDuration = '0.4s';
 
   //back wings are getting faster
-  if (event.key === "ArrowUp") {
+  if (event.key === 'ArrowUp') {
     drone1WingLeft.style.animationDuration = newDuration;
     drone1WingRight.style.animationDuration = newDuration;
   }
   //front wings are getting faster
-  else if (event.key === "ArrowDown") {
+  else if (event.key === 'ArrowDown') {
     drone2WingRight.style.animationDuration = newDuration;
     drone2WingLeft.style.animationDuration = newDuration;
   }
   //left wings are getting faster
-  else if (event.key === "ArrowRight") {
+  else if (event.key === 'ArrowRight') {
     drone1WingLeft.style.animationDuration = newDuration;
     drone2WingLeft.style.animationDuration = newDuration;
   }
   //right wings are getting faster
-  else if (event.key === "ArrowLeft") {
+  else if (event.key === 'ArrowLeft') {
     drone1WingRight.style.animationDuration = newDuration;
     drone2WingRight.style.animationDuration = newDuration;
   }
 
-  document.addEventListener("keyup", function (event) {
+  document.addEventListener('keyup', function (event) {
     drone1WingRight.style.animationDuration = defaultDuration;
     drone1WingLeft.style.animationDuration = defaultDuration;
     drone2WingRight.style.animationDuration = defaultDuration;
