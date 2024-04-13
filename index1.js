dbc0e7750b486d4b8179964eb834cc1aaa64f086
arevikmosinyan <aarreevviikk1@gmail.com>
2024-04-14 02:13:17 +0400

    Merge branch 'master' of https://github.com/arevikmosinyan/Drone

--- index1.js
+++ index1.js
@@ -2,10 +2,10 @@ let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
 let dronesContainer = document.getElementById('dronesContainer');
 let positionX = 0;
-let positionY = 20;
+let positionY = 30;
 let bigger = false;
 let smaller = false;
-let rotationDegree = 0;
+
 let scaled = false;
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -22,159 +22,92 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   };
 });
-
+let rotationDegree = 0;
+let scaledDegree = 0;
+let newRotationDegree = 0;
+let count = 0;
+let circleCount = 0;
 document.addEventListener('keydown', function (event) {
   const step = 7;
 
-  /*-------------------------------making bigger and smaller to make the drone going up and landing-------------------------*/
+  /*-------------------------------Landing and taking off the Drone--------------------------------------------------------*/
 
   if (event.key === ' ' || event.key === '+') {
     if (smaller) {
-      dronesContainer.style.transform = 'scale(1)';
+      // dronesContainer.style.transform = 'scale(1)';
+      dronesContainer.style.transform = `scale(1) rotate(${rotationDegree}deg)`;
+      scaled = false;
       smaller = false;
     } else {
-      dronesContainer.style.transform = 'scale(1.2)';
+      // dronesContainer.style.transform = 'scale(1.2)';
+      dronesContainer.style.transform = `scale(1.2) rotate(${rotationDegree}deg)`;
       bigger = true;
       scaled = true;
     }
+  } else if (event.key === 'ArrowUp' || event.key === 'w') {
+  } else if (event.key === 'ArrowDown' || event.key === 's') {
   } else if (event.key === 'Control' || event.key === '-') {
     if (bigger) {
-      dronesContainer.style.transform = 'scale(1)';
+      // dronesContainer.style.transform = 'scale(1)';
+      dronesContainer.style.transform = `scale(1) rotate(${rotationDegree}deg)`;
+
+      scaled = false;
       bigger = false;
     } else {
-      dronesContainer.style.transform = 'scale(0.4)';
+      // dronesContainer.style.transform = 'scale(0.4)';
+      dronesContainer.style.transform = `scale(0.4) rotate(${rotationDegree}deg)`;
       smaller = true;
       scaled = true;
     }
   }
+
   /*---------------------------------------------------------------------------------------------------------------------------*/
 
   /*-------------------------------------------rotating the drone--------------------------------------------------------------*/
 
-  let startIndex = dronesContainer.style.transform.indexOf('(') + 1;
-  let endIndex = dronesContainer.style.transform.indexOf('deg');
-  let rotationDegreeString = dronesContainer.style.transform.slice(
-    startIndex,
-    endIndex,
+  // let startIndex = dronesContainer.style.transform.indexOf('(') + 1;
+  // let endIndex = dronesContainer.style.transform.indexOf('deg');
+  // let rotationDegreeString = dronesContainer.style.transform.slice(
+  //   startIndex,
+  //   endIndex,
+  // );
+  // let rotationDegree = Number(rotationDegreeString);
+
+  /*-----------------------------------------finding out if the drone is scaled or not, to make it moved with scaled transformation-------*/
+  const startIndexOfScaleValue =
+    dronesContainer.style.transform.indexOf('scale(');
+  const endIndexOfScaleValue = dronesContainer.style.transform.indexOf(')');
+  const scaledSizeString = dronesContainer.style.transform.slice(
+    startIndexOfScaleValue + 'scale('.length,
+    endIndexOfScaleValue,
   );
-  let rotationDegree = Number(rotationDegreeString);
+
+  /*-------------------------------------------------------------------------------------------------------------------------------------*/
   // rotate the drone to the left
+  // let count = 0;
 
   if (event.key === 'q') {
-    let rotationDegree = 0;
-    findRotation();
-    // let startIndex = dronesContainer.style.transform.indexOf("(") + 1;
-    // let endIndex = dronesContainer.style.transform.indexOf("deg");
-    // let rotationDegreeString = dronesContainer.style.transform.slice(
-    //   startIndex,
-    //   endIndex
-    // );
-    // rotationDegree = Number(rotationDegreeString);
-
-    /*-----------------------------------------finding out if the drone is scaled or not, to make it moved with scaled transformation-------*/
-
-    const startIndexOfScaleValue =
-      dronesContainer.style.transform.indexOf('scale(');
-
-    const endIndexOfScaleValue = dronesContainer.style.transform.indexOf(')');
-
-    const scaledSizeString = dronesContainer.style.transform.slice(
-      startIndexOfScaleValue + 'scale('.length,
-      endIndexOfScaleValue,
-    );
-
-    // console.log("Scaled size string:", scaledSizeString);
-
-    /*-------------------------------------------------------------------------------------------------------------------------------------*/
-
-    function findRotation() {
-      rotationDegree = Math.abs(rotationDegree) + 90;
-
-      return -rotationDegree;
+    count += 1;
+    if (count === 4) {
+      circleCount += 1;
+      count = 0;
     }
-    console.log(findRotation());
-    findRotation();
-    dronesContainer.style.transform = `rotate(${findRotation()})`;
-    dronesContainer.style.transform;
-    console.log(
-      dronesContainer.style.transform + ' dronesContainer.style.transform ',
-    );
-    // if (rotationDegree === 0) {
-    //   dronesContainer.style.transform = "rotate(-90deg)";
+    rotationDegree -= 90;
 
-    //   rotationDegree = -90;
-    // } else if (rotationDegree === -90) {
-    //   dronesContainer.style.transform = "rotate(-180deg)";
-
-    //   rotationDegree = -180;
-    // } else if (rotationDegree === -180) {
-    //   dronesContainer.style.transform = "rotate(-270deg)";
-
-    //   rotationDegree = -270;
-    // } else if (rotationDegree === -270) {
-    //   dronesContainer.style.transform = "rotate(-360deg)";
-
-    //   rotationDegree = -360;
-    // } else if (rotationDegree === -360) {
-    //   rotationDegree = 0;
-    //   console.log(rotationDegree);
-    //   dronesContainer.style.transform = "rotate(-450deg)";
-    // }
+    if (scaled) {
+      dronesContainer.style.transform = `scale(${scaledSizeString}) rotate(${rotationDegree}deg)`;
+    } else {
+      dronesContainer.style.transform = `rotate(${rotationDegree}deg)`;
+    }
   }
-
-  //   if (rotationDegree === 0) {
-  //     dronesContainer.style.transform = scaled
-  //       ? `rotate(-90deg) scale(${scaledSizeString}) `
-  //       : "rotate(-90deg)";
-
-  //     rotationDegree = -90;
-  //   } else if (rotationDegree === -90) {
-  //     dronesContainer.style.transform = scaled
-  //       ? `rotate(-180deg) scale(${scaledSizeString})`
-  //       : "rotate(-180deg)";
-
-  //     rotationDegree = -180;
-  //   } else if (rotationDegree === -180) {
-  //     dronesContainer.style.transform = scaled
-  //       ? `rotate(-270deg) scale(${scaledSizeString})`
-  //       : "rotate(-270deg)";
-
-  //     rotationDegree = -270;
-  //   } else if (rotationDegree === -270) {
-  //     dronesContainer.style.transform = scaled
-  //       ? `rotate(-360deg) scale(${scaledSizeString})`
-  //       : "rotate(-360deg)";
-
-  //     rotationDegree = -360;
-  //   } else if (rotationDegree === -360) {
-  //     rotationDegree = 0;
-  //   }
-  // }
 
   //rotate the drone to the right
   if (event.key === 'e') {
-    let startIndex = dronesContainer.style.transform.indexOf('(') + 1;
-    let endIndex = dronesContainer.style.transform.indexOf('deg');
-    let rotationDegreeString = dronesContainer.style.transform.slice(
-      startIndex,
-      endIndex,
-    );
-    rotationDegree = Number(rotationDegreeString);
-
-    if (rotationDegree === 0) {
-      dronesContainer.style.transform = 'rotate(90deg)';
-      rotationDegree = 90;
-    } else if (rotationDegree === 90) {
-      dronesContainer.style.transform = 'rotate(180deg)';
-      rotationDegree = 180;
-    } else if (rotationDegree === 180) {
-      dronesContainer.style.transform = 'rotate(270deg)';
-      rotationDegree = 270;
-    } else if (rotationDegree === 270) {
-      dronesContainer.style.transform = 'rotate(360deg)';
-      rotationDegree = 360;
-    } else if (rotationDegree === 360) {
-      rotationDegree = 0;
+    rotationDegree += 90;
+    if (scaled) {
+      dronesContainer.style.transform = `scale(${scaledSizeString}) rotate(${rotationDegree}deg)`;
+    } else {
+      dronesContainer.style.transform = `rotate(${rotationDegree}deg)`;
     }
   }
   /*-------------------------------------------------------------------------------------------------------------------------*/
@@ -301,22 +234,22 @@ document.addEventListener('keydown', function (event) {
   let defaultDuration = '0.4s';
 
   //back wings are getting faster
-  if (event.key === 'ArrowUp') {
+  if (event.key === 'ArrowUp' || event.key === 'w') {
     drone1WingLeft.style.animationDuration = newDuration;
     drone1WingRight.style.animationDuration = newDuration;
   }
   //front wings are getting faster
-  else if (event.key === 'ArrowDown') {
+  else if (event.key === 'ArrowDown' || event.key === 's') {
     drone2WingRight.style.animationDuration = newDuration;
     drone2WingLeft.style.animationDuration = newDuration;
   }
   //left wings are getting faster
-  else if (event.key === 'ArrowRight') {
+  else if (event.key === 'ArrowRight' || event.key === 'd') {
     drone1WingLeft.style.animationDuration = newDuration;
     drone2WingLeft.style.animationDuration = newDuration;
   }
   //right wings are getting faster
-  else if (event.key === 'ArrowLeft') {
+  else if (event.key === 'ArrowLeft' || event.key === 'a') {
     drone1WingRight.style.animationDuration = newDuration;
     drone2WingRight.style.animationDuration = newDuration;
   }
